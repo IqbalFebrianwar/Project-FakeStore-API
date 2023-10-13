27b7e64ad491f6bca27b9850f88b35536f8f08b4
IqbalFebrianwar <zonelyrics26@gmail.com>
2023-10-13 18:05:42 +0700

    Menambahkan suspend loading

---
+++ src/app/products/loading.jsx
@@ -0,0 +1,8 @@
+const loading =()=>{
+    return(
+        <div className="w-full h-screen flex justify-center items-center">
+            <h1 className="text-2xl text-bold">Loading ...</h1>
+        </div>
+    )
+}
+export default loading
